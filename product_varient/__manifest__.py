1616335ecf6702b117f5e0f6ba86dfce5ac600d0
sangita <sbacchewar@dexciss.com>
2019-08-17 12:41:14 +0530

    product varent issue

--- product_varient/__manifest__.py
+++ product_varient/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Product Varient",
-    "version": "10.0.0.0",
-    "description": """
+    "version": "10.0.0.1",
+    "description": """Use domain in paython
     """,
     "author": "Dexciss Technology Pvt Ltd. (Sangita)",
     'website': "http://www.dexciss.com",
--- product_varient/model/poduct_varient.py
+++ product_varient/model/poduct_varient.py
@@ -6,20 +6,31 @@ class SaleOrderLine(models.Model):
     
     prod_temp_id = fields.Many2one('product.template',string="Product",required=True)
     attribute_id = fields.Many2one('product.attribute',string="Attribute")
-    value_ids = fields.Many2one('product.attribute.value',string="Attribute Value",required=True)
+    value_ids = fields.Many2one('product.attribute.value',string="Attribute Value")
+#     dynamic_domain_ids = fields.Many2many('product.attribute.value',string="Dynamic")
     
     @api.onchange('prod_temp_id')
     def onchange_prod_temp_id(self):
         for s in self:
             for line in s.prod_temp_id.attribute_line_ids:
                 s.attribute_id = line.attribute_id.id
-                
+            if not s.attribute_id:
+                product_id = self.env['product.product'].search([('product_tmpl_id','=',self.prod_temp_id.id)])
+#                 print("_______---product_idproduct_idproduct_id-----------------------",product_id)
+                for product in product_id:
+                    s.product_id = product
+                    
+            attr = self.env['product.attribute.value']
+            for attributes in s.prod_temp_id.attribute_line_ids:
+                attr += attributes.mapped('value_ids')
+            return {'domain':{'value_ids':[('id','in',attr.ids)]}}
+            
     @api.onchange('value_ids')
     def onchange_value_ids(self):
         for s in self:
             for line in s.value_ids.product_ids:
                 s.product_id = line.id
-
+                
 class ProductAttributeValue(models.Model):
     _inherit = "product.attribute.value"
 
